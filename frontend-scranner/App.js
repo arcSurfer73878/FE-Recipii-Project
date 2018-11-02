24bf0904c8ace2f62927239abc41dc6b994721e0
Kaiming Shen <kaimingshen@Kaimings-MacBook-Pro.local>
2018-11-02 16:49:51 +0000

    fixed props parent to child

--- frontend-scranner/App.js
+++ frontend-scranner/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View, ImageBackground, Button } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
-import { createBottomTabNavigator, createSwitchNavigator } from 'react-navigation'
+import { createBottomTabNavigator, createSwitchNavigator, createStackNavigator } from 'react-navigation'
 import HomeScreen from "./navigation/HomePage"
 import PostScreen from './navigation/PostPage'
 import RecipeScreen from "./navigation/RecipePage"
@@ -12,10 +12,32 @@ import Form from './components/Form'
 import Logo from './components/Logo'
 import SignupSection from './components/SignupSection'
 import ButtonSubmit from './components/ButtonSubmit'
+import UserPage from './navigation/UserPage';
 
 export default class App extends React.Component {
   state = {
     login: false
+  }
+  constructor(props) {
+    super(props)
+    state = {
+      user: {
+        _id: "5bd73ddfbf9a0930d7f77b15",
+        username: "emmajarvis",
+        firstName: "Emma",
+        lastName: "Jarvis",
+        email: "emmajarvis@gmail.com",
+        profilePicture: "",
+        address: {
+          houseNumber: "",
+          street: "",
+          city: "",
+          postcode: "",
+          telephone: "",
+        },
+        __v: 0
+      }
+    }
   }
   render() {
     return !this.state.login ?
@@ -31,8 +53,6 @@ export default class App extends React.Component {
         <ButtonSubmit login={this.changeView.bind(this)} />
       </ImageBackground>
       ) : (<AppNavigator />)
-
-
   }
   changeView() {
     this.setState({
@@ -43,18 +63,18 @@ export default class App extends React.Component {
 
 const BottomNavigator = createBottomTabNavigator({
   Home: HomeScreen,
-  Recipe: RecipeScreen,
+  Post: PostScreen,
   Basket: BasketScreen
 })
-
 const AppNavigator = createSwitchNavigator({
   Bottom: BottomNavigator,
-  User: UserScreen,
+  User: {
+    screen: props => <UserScreen {...props} user={this.state.user} />
+  },
   Camera: CameraScreen,
   Post: PostScreen,
   App,
 })
-
 
 const styles = StyleSheet.create({
   container: {
