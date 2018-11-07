391fe398fae6446c969f7a61f6b0745b9a8dab2b
Rachael Hall <rachael.hall.889@gmail.com>
2018-11-07 16:27:51 +0000

    add recipie button

--- frontend-scranner/navigation/ConfirmationPage.js
+++ frontend-scranner/navigation/ConfirmationPage.js
@@ -32,7 +32,6 @@ export default class ConfirmationPage extends Component {
             onPress: () => this.props.navigation.navigate("User")
           }}
         />
-        {console.log(this.state.ingredients)}
         <View>
           <TextInput value={this.state.title} onChangeText={(text) => this.updateText(text, title)} />
           <TextInput value={this.state.servings} onChangeText={(text) => this.updateText(text, servings)} />
@@ -52,8 +51,9 @@ export default class ConfirmationPage extends Component {
           <TextInput value={this.state.newIngredient.name} onChangeText={(text) => this.updateText(text, { newIngredient: name })} />
           <TextInput value={this.state.newIngredient.amount} onChangeText={(text) => this.updateText(text, { newIngredient: amount })} />
           <TextInput value={this.state.newIngredient.units} onChangeText={(text) => this.updateText(text, { newIngredient: units })} />
-          <Button title='Add' onPress={this.addIngredient} />
+          <Button title='Add Ingredient' onPress={this.addIngredient} />
         </View>
+        <Button title='Confirm Recipe' onPress={this.addRecipe} />
       </View>
     )
   }
@@ -78,4 +78,44 @@ export default class ConfirmationPage extends Component {
       ingredients: newIngredients
     });
   }
+
+  addNewRecipe = (ingredients, title, servings) => {
+    const api = new Frisbee({
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    });
+
+    const ingredientList = ingredients.reduce((acc, ingredient) => {
+      if (ingredient.body.length > 0)
+        acc.push({
+          foodType: ingredient.body[0].aisle,
+          name: ingredient.body[0].name,
+          amount: ingredient.body[0].amount,
+          units: ingredient.body[0].unit,
+          price: ingredient.body[0].estimatedCost.value
+        });
+      return acc;
+    }, []);
+
+    const request = {
+      name: title,
+      servings,
+      ingredients: ingredientList
+    };
+
+    console.log(request, "<<<<<<<<<<<<<<<<");
+    api
+      .post(
+        `https://scranner123.herokuapp.com/api/recipes/5be2fdbc6333b741ca036886`,
+        { body: request }
+      )
+      .then(response => {
+        console.log(response)
+        this.props.navigation.navigate("Home")
+      });
+  };
+}
+
 };
